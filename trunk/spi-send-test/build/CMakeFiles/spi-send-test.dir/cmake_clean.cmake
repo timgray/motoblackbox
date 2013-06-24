244a075a87b6705fc352f828a9dc54374dc10e4d
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@2a1c984a-e5f1-9a1f-0334-f1065f932f1a>
2013-06-24 15:27:43 +0000

    Share project "spi-send-test" into "https://motoblackbox.googlecode.com/svn"

---
+++ trunk/spi-send-test/build/CMakeFiles/spi-send-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/spi-send-test.dir/home/iwasz/Documents/workspace-CDT/spi-send-test/src/main.c.o"
+  "CMakeFiles/spi-send-test.dir/home/iwasz/Documents/workspace-CDT/spi-send-test/src/dallas_one_wire.c.o"
+  "spi-send-test.pdb"
+  "spi-send-test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/spi-send-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
