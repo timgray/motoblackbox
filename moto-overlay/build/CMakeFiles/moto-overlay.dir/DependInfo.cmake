56f6582928a7fa06e827acbbf000256646481a3c
lukasz.iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2013-06-24 15:25:06 +0000

    Share project "moto-overlay" into "https://motoblackbox.googlecode.com/svn"

---
+++ moto-overlay/build/CMakeFiles/moto-overlay.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/iwasz/Documents/workspace-CDT/moto-overlay/src/main.cc" "/home/iwasz/Documents/workspace-CDT/moto-overlay/build/CMakeFiles/moto-overlay.dir/home/iwasz/Documents/workspace-CDT/moto-overlay/src/main.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/iwasz/local/include"
+  "/home/iwasz/local/include/freetype2"
+  "/home/iwasz/local/include/libpng15"
+  "/usr/include/cairo"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "/usr/include/pixman-1"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
