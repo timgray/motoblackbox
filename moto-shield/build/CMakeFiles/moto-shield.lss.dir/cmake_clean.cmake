de44b9b8c83410ca0f9c4ce60605eec4c801248d
lukasz.iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2013-06-24 15:20:29 +0000

    fgdyhhgn

---
+++ moto-shield/build/CMakeFiles/moto-shield.lss.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/moto-shield.lss"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/moto-shield.lss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
