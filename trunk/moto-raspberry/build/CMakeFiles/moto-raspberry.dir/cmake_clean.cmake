a7bcda71a0a24a297340d2d5ab220013526652a8
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@2a1c984a-e5f1-9a1f-0334-f1065f932f1a>
2013-06-24 15:21:41 +0000

    fgdyhhgn

---
+++ trunk/moto-raspberry/build/CMakeFiles/moto-raspberry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/moto-raspberry.dir/home/iwasz/Documents/workspace-CDT/moto-raspberry/src/main.cc.o"
+  "moto-raspberry.pdb"
+  "moto-raspberry"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/moto-raspberry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
