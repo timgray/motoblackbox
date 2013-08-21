4d942901d7494fe82b376caaa3f91ee412b04f32
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@2a1c984a-e5f1-9a1f-0334-f1065f932f1a>
2013-08-21 20:18:11 +0000

    svn delete and ignore

--- trunk/moto-shield/build/CMakeFiles/moto-shield.hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/moto-shield.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/moto-shield.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- trunk/moto-shield/build/CMakeFiles/moto-shield.lss.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/moto-shield.lss"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/moto-shield.lss.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
