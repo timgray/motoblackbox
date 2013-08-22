1cfe42505b09f398e59074eb0b5d92cfa66d57ef
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@2a1c984a-e5f1-9a1f-0334-f1065f932f1a>
2013-08-22 21:55:11 +0000

    files

--- trunk/moto-raspberry/build/raspberrypi.cmake
+++ trunk/moto-raspberry/build/raspberrypi.cmake
@@ -1,12 +1,18 @@
 SET(CMAKE_SYSTEM_NAME Generic)
 
-SET(CMAKE_C_COMPILER /home/iwasz/local/share/raspi-gcc/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER /home/iwasz/local/share/raspi-gcc/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-g++)
+#SET(CMAKE_C_COMPILER /home/iwasz/local/share/raspi-gcc/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-gcc)
+#SET(CMAKE_CXX_COMPILER /home/iwasz/local/share/raspi-gcc/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-g++)
 
-#SET(CFLAGS "-O3 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s")
-#SET(CXXFLAGS "-O3 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s")
-SET(CFLAGS "-O3 -march=armv6zk -mtune=arm1176jzf-s")
-SET(CXXFLAGS "-O3 -march=armv6zk -mtune=arm1176jzf-s")
+#SET(CMAKE_C_COMPILER /home/iwasz/local/share/raspi-linaro/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-gcc)
+#SET(CMAKE_CXX_COMPILER /home/iwasz/local/share/raspi-linaro/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-g++)
+
+SET(CMAKE_C_COMPILER /home/iwasz/local/share/raspi-linaro/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER /home/iwasz/local/share/raspi-linaro/tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi-g++)
+
+#SET(CFLAGS "-mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -pthread")
+#SET(CXXFLAGS "-mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -std=c++11 -pthread")
+SET(CFLAGS "-mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -static-libgcc -pthread")
+SET(CXXFLAGS "-mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -static-libstdc++ -static-libgcc -std=c++11 -pthread")
 
 SET(CMAKE_C_FLAGS  ${CFLAGS})
 SET(CMAKE_CXX_FLAGS ${CXXFLAGS})
